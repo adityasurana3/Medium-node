7337e16eef9231a6e53b2de26c4cf73eff74a68b
Aditya Surana <aditya.surana@proshore.eu>
2024-12-26 17:04:35 +0545

    common folder created for backed and frontend

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -80,14 +80,14 @@ blogRouter.get('/bulk', async (c) => {
     return c.json({'blogs': blogs})
 })
 
-blogRouter.get('/', async (c) => {
-    const body = await c.req.json()
+blogRouter.get('/:id', async (c) => {
+    const id = c.req.param("id")
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
     const blog = await prisma.post.findFirst({
         where: {
-            id: body.id
+            id: id
         }
     })
     return c.json({'blog': blog})
