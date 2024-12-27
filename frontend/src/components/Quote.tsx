50656f2372167636b2eb3e31da9a90bdfc6f489d
Aditya Surana <aditya.surana@proshore.eu>
2024-12-27 20:27:09 +0545

    frontend added

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = () =>{
+    return (
+        <div className="flex flex-col justify-center h-screen bg-slate-200">
+            <div className="flex justify-center">
+                <div className="max-w-lg">
+                    <div className="text-3xl font-bold">
+                    "The customer support I received was exceptional. The support team want above and beyond to address my concerns"
+                    </div>
+                    <div className="max-w-md text-xl font-semibold text-left mt-4">
+                        Julies Winfield
+                    </div>
+                    <div className="max-w-md text-sm font-light text-slate-400">
+                        CEO | Acme Corp
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
